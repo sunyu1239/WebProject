07aed83daa5e3e80f69d0ff8dce0a9871e35d73f
sunyu1239 <sunyu1239@yahoo.com>
2019-05-22 16:33:51 +0800

    0522

--- src/com/example/day0520/bean/TestBeans.java
+++ src/com/example/day0520/bean/TestBeans.java
@@ -1,5 +1,26 @@
 package com.example.day0520.bean;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestBeans {
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
+        User u = (User) ac.getBean("user");
+        System.out.println(u.getName());
+        System.out.println(u.getAge());
+        for (String s : u.getAddress()) {
+            System.out.println(s);
+        }
+        for (Lesson les : u.getLessonList()) {
+            System.out.println(les.getId() + ":" + les.getName());
+        }
+        for (Person p : u.getPersonSet()) {
+            System.out.println(p.getAge() + ":" + p.getName());
+        }
+        for (String key : u.getMap().keySet()) {
+            System.out.println(key + ":" + u.getMap().get(key).getId() + ":" + u.getMap().get(key).getName());
+        }
+    }
 
 }
